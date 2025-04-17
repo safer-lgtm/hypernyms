09b549c089a0a6ff31f0a67e3d553fca69084e16
safer-lgtm <git@code.fbi.h-da.de/safouan.erryfy>
2025-04-18 00:19:50 +0200

    extract actions prompt

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from src.model import LLMHypernymExtractor
+from src.data_loader import load_emails, process_llama_output
+from src.preprocessing import TextPreprocessor
+from src.config import DATA_PATH
 
 def main():
     # Initialisierung
     llm = LLMHypernymExtractor()
+    preprocessor = TextPreprocessor()
+
+    # Daten laden
+    df = load_emails(DATA_PATH)
+    print(df.shape)
+
+    # Aktionen extrahieren
+    df["json"] = llm.extract_actions(df["file_texts"].tolist())
+    df = process_llama_output(df)
+
+    # Lemmatisierung
+    df = preprocessor.lemmatize_column(df, "Action")
+    df = preprocessor.lemmatize_column(df, "Object")
+
+    # Hypernym-Analyse
+    unique_objects = df["objectlemma"].dropna().unique()
+    print(unique_objects)
 
 if __name__ == "__main__":
     main()
--- src/model.py
+++ src/model.py
@@ -1,6 +1,6 @@
 from transformers import pipeline
 import torch
-from src.config import MODEL_NAME, HF_TOKEN
+from src.config import MODEL_NAME, HF_TOKEN, MAX_NEW_TOKENS, BATCH_SIZE
 
 
 class LLMHypernymExtractor:
@@ -15,3 +15,38 @@ class LLMHypernymExtractor:
         self.pipeline.tokenizer.pad_token_id = self.pipeline.model.config.eos_token_id[0]
         self.pipeline.tokenizer.padding_side = "left"
         self.pipeline.model.generation_config.pad_token_id = self.pipeline.tokenizer.pad_token_id
+
+    def extract_actions(self, texts):
+        messages = []
+        for text in texts:
+            messages.append([{
+                "role": "system",
+                "content":
+                    """
+                    Extrahiere aus der neuesten E-Mail an den Empfänger die Aktion (Verb im Infinitiv) und das Objekt (Substantiv).
+            
+                    Format: [AKTION, OBJEKT]
+                    Keine Erklärungen oder Zusatzinfos.
+            
+                    Regeln:
+                    - Die Antwort muss auf Deutsch sein und das Objekt ein Substantiv sein.
+                    - Falls keine klare Aufgabe bzw. passende [AKTION, OBJEKT] erkennbar ist, bitte: [UNBEKANNT, UNBEKANNT] zurückgeben.
+            
+                    Beispiele:
+                    [antworten, E-Mail]
+                    [halten, Votrag]
+                    [senden, Dokument]
+                    [anmelden, Projekt]
+                    [weiterleiten, Info]
+                    """
+            }, {
+                "role": "user",
+                "content": f"\"{text}\""
+            }])
+
+        outputs = self.pipeline(
+            messages,
+            max_new_tokens=MAX_NEW_TOKENS,
+            batch_size=BATCH_SIZE
+        )
+        return [output[0]["generated_text"][-1]["content"].strip() for output in outputs]
